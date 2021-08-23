96a87d665940b1bda91fab32157e9e29d98139f3
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-08-23 14:36:48 +0900

    하단 네비게이션바 생성

---
+++ src/components/bottom/Bottom.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Bottom.module.css';
+import { Home, Visibility, AllInbox, TransferWithinAStation } from '@material-ui/icons'
+import { useHistory } from 'react-router-dom';
+
+function Bottom() {
+    const history = useHistory();
+    return (
+        <div className={styles.container}>
+            <div className={styles.iconContainer} onClick={() => history.push('/menu')}>
+                <Visibility className={styles.lockIcon} />
+                <p className={styles.iconText}>카메라</p>
+            </div>
+            <div className={styles.iconContainer}>
+                <TransferWithinAStation className={styles.lockIcon} />
+                <p className={styles.iconText}>출입 기록</p>
+            </div>
+            <div className={styles.iconContainer}>
+                <AllInbox className={styles.lockIcon} />
+                <p className={styles.iconText}>택배</p>
+            </div>
+            <div className={styles.iconContainer}>
+                <Home className={styles.lockIcon} onClick={() => history.push('/')} />
+                <p className={styles.iconText}>메인</p>
+            </div>
+        </div>
+    )
+}
+
+export default Bottom;
