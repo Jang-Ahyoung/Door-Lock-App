92890271954ae8fe6177506b4899ab248b7f0288
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-08-24 15:11:03 +0900

    🚂 페이지 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SettingView from './components/settingView/SettingView';
+import Home from './page/home/Home';
+import Menu from './page/menu/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/setting" component={SettingView} />
+          <Route path="/menu/camera"><Menu type={"camera"} /></Route>
+          <Route path="/menu/access"><Menu type={"access"} /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
