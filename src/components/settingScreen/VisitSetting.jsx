b71caf9429efb36ab0468f6698f3a06a17e40d47
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-09-08 20:58:35 +0900

    modify setting screen

--- src/components/settingScreen/VisitSetting.jsx
+++ src/components/settingScreen/VisitSetting.jsx
@@ -8,7 +8,7 @@ function VisitSetting() {
     return (
         <div className={styles.container}>
             <div className={styles.settingBox}>
-                <div className={styles.middle}>
+                <div className={styles.boxpadding}>
                     <section className={styles.mainTitle}><Face />
                         <span>가구원</span>
                         <button>편집</button>
@@ -37,34 +37,39 @@ function VisitSetting() {
             <div className={styles.settingBox}>
                 <p className={styles.subTitle}>임시 비밀번호 관리</p>
                 <div className={styles.middle}>
-                    <div className={styles.boxContent}>
-                        <div>
+                    <div className={styles.postContent}>
+                        <div className={styles.detailContent}>
                             <div className={styles.text}>
                                 <Https />
                                 <p className={styles.title}>Test 비밀번호</p>
                             </div>
-                            <p className={styles.detail}><EventAvailable className={styles.miniIcon} /><span>08월 05일 ~ 07일</span> <WatchLater className={styles.miniIcon} />10:00 - 12:00</p>
-                            <p className={styles.detail}>Test Password  _  123456 </p>
+                            <div className={styles.controlBox} onClick={() => setControl(!control)}>
+                                <div className={styles.line}></div>
+                                <div className={`${styles.circle} ${!control && styles.move}`} ></div>
+                            </div>
                         </div>
-                        <div className={styles.controlBox} onClick={() => setControl(!control)}>
-                            <div className={styles.line}></div>
-                            <div className={`${styles.circle} ${!control && styles.move}`} ></div>
+                        <div>
+                            <p className={styles.detail}><EventAvailable className={styles.miniIcon} /><span>08월 05일 ~ 07일</span></p>
+                            <p className={styles.detail}><WatchLater className={styles.miniIcon} />10:00 - 12:00</p>
+                            <p className={styles.detail}>Test Password  <span className={styles.pwNum}>123456</span></p>
                         </div>
                     </div>
                     <hr className={styles.sectionLine} />
-                    <div className={styles.boxContent}>
-                        <div>
+
+                    <div className={styles.postContent}>
+                        <div className={styles.detailContent}>
                             <div className={styles.text}>
                                 <Https />
                                 <p className={styles.title}>Test 비밀번호 2</p>
                             </div>
-                            <p className={styles.detail}><EventAvailable className={styles.miniIcon} /><span>08월 05일 ~ 10일</span> <WatchLater className={styles.miniIcon} />13:00 - 15:00</p>
-                            <p className={styles.detail}>Test Password  _  789101 </p>
+                            <div className={styles.controlBox} onClick={() => setPassword(!password)}>
+                                <div className={styles.line}></div>
+                                <div className={`${styles.circle} ${!password && styles.move}`} ></div>
+                            </div>
                         </div>
-                        <div className={styles.controlBox} onClick={() => setPassword(!password)}>
-                            <div className={styles.line}></div>
-                            <div className={`${styles.circle} ${!password && styles.move}`} ></div>
-                        </div>
+                        <p className={styles.detail}><EventAvailable className={styles.miniIcon} /><span>08월 05일 ~ 10일</span></p>
+                        <p className={styles.detail}><WatchLater className={styles.miniIcon} />13:00 - 15:00</p>
+                        <p className={styles.detail}>Test Password <span className={styles.pwNum}>789101</span> </p>
                     </div>
                 </div>
             </div>
